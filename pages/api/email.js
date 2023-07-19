ddd58190dc6c22c18eb3d347b99ec8d69715fe48
Ezeko95 <ciangianluca@hotmail.com>
2023-07-19 12:43:46 -0300

    footer

---
+++ pages/api/email.js
@@ -0,0 +1,39 @@
+import nodemailer from "nodemailer";
+import bodyParser from "body-parser";
+require("dotenv").config();
+
+const transporter = nodemailer.createTransport({
+  service: "Hotmail",
+  auth: {
+    user: process.env.EMAIL_ADDRESS,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
+
+export default function handler(req, res) {
+  console.log("arrived puto")
+  if (req.method === "POST") {
+    bodyParser.urlencoded({ extended: true })(req, res, () => {
+      const { email } = req.body;
+      // Email content
+      const mailOptions = {
+        from: process.env.EMAIL_ADDRESS, 
+        to: "ciangianluca@hotmail.com", 
+        subject: "Portfolio Subscriber!",
+        text: `New email subscriber: ${email}`,
+      };
+
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.log(error);
+          res.status(500).json({ error: "Failed to send email API" });
+        } else {
+          console.log("Email sent: " + info.response);
+          res.status(200).json({ message: "Email sent successfully" });
+        }
+      });
+    });
+  } else {
+    res.status(405).end();
+  }
+}
