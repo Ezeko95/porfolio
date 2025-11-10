213378b3d3041da0c4d52de5b232625a8869fc4b
ezeko95 <ciangianluca@hotmail.com>
2025-11-10 10:30:09 -0300

    updated layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://gianluca-cianchi.com.ar"),
   title: {
     default: "Portfolio - Desarrollador Full-Stack",
     template: "%s | Portfolio",
